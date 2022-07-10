cbb1e4197bc327e25f9e82e098c7e5005e494172
pooja1234K <poojakaushal18@gmail.com>
2022-07-10 16:26:51 +1200

    first commit

--- poojatest1/poojatest1/Program.cs
+++ poojatest1/poojatest1/Program.cs
@@ -22,8 +22,8 @@ usernameTexbox.SendKeys("hari");
 
 // identify password textbox and enter valid password
 
-IWebElement usernamePassword = driver.FindElement(By.Id("Password"));
-usernamePassword.SendKeys("123123");
+IWebElement PasswordTextbox = driver.FindElement(By.Id("Password"));
+PasswordTextbox.SendKeys("123123");
 
 // identify login button and click on it
 
@@ -33,14 +33,15 @@ loginButton.Click();
 // check if the user is loggedin succesfully
 
 IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+Thread.Sleep(1000);
 
-if
-(helloHari.Text == "hello hari!")
-{ Console.WriteLine("logged in successfully ");
+if(helloHari.Text == "Hello hari!")
+{ 
+Console.WriteLine("logged in successfully ");
 }
 else
 {
-	Console.WriteLine(" login failed");
+Console.WriteLine(" login failed");
 }
 // -- create a new material record -- //
 
@@ -89,11 +90,101 @@ IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
 saveButton.Click();
 
 // go to the last page
-
+Thread.Sleep(2000);
 IWebElement gotothelastPageButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
 gotothelastPageButton.Click();
 
 // Check if the new material is created successfully
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+if (newCode.Text == "poojatest1")
+{
+	Console.WriteLine(" new material is created successfully");
+}
+else
+{
+	Console.WriteLine(" new material is failed");
+}
+
+// -- check if the edit button is working --//
+
+// Click on edit button
+Thread.Sleep(3000);
+IWebElement EditButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr/td[5]/a[1]"));
+EditButton.Click();
+
+// edit the code textbox
+IWebElement newcodeTextbox = driver.FindElement(By.Id("Code"));
+newcodeTextbox.Clear();
+newcodeTextbox.SendKeys("poojatest2");
+
+// edit the description textbox
+IWebElement newdesciptionTextbox = driver.FindElement(By.Id("Description"));
+newdesciptionTextbox.Clear();
+newdesciptionTextbox.SendKeys("poojatest2");
+
+
+// price tag interactable
+IWebElement newpriceTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span"));
+newpriceTag.Click();
+
+// edit price per unit textbox
+IWebElement newPrice = driver.FindElement(By.Id("Price"));
+
+newPrice.SendKeys("20");
+
+// click on save button
+IWebElement newsaveButton = driver.FindElement(By.Id("SaveButton"));
+newsaveButton.Click();
+
+// -- Delete the record --//
+
+// click on delete button
+IWebElement deleteButton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[1]/td[5]/a[2]"));
+deleteButton.Click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// edit the descrption textbox
+
+
+
+
+
+
+
+
+
+
 
 
 
